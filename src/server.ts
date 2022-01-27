39b7f388d75e8b07bef4634c71b8473eee85aae1
Josue Zolakio <josuezolakiocanal@gmail.com>
2022-01-27 19:46:56 +0100

    New swagger.json created and tmp files deleted

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
 import express from 'express';
+import { setup, serve } from 'swagger-ui-express';
 import { config } from 'dotenv';
 import cors from 'cors';
+
 import { router } from './routes';
+import swaggerFile from './swagger.json';
 
 const app = express();
 config({ path: '.env' });
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api-docs', serve, setup(swaggerFile));
 
 app.use(router);
 
